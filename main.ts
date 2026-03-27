b462495231db4d0c05d9ec39092a72742dc50388
Luhle Mahlangu <125449200+LuhleMahlangu@users.noreply.github.com>
2026-03-27 09:24:27 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P0, 90)
+    // Servo LEFT = gate closes
+    pins.servoWritePin(AnalogPin.P0, 0)
+    basic.showString("<")
 })
 input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P0, 0)
+    // Servo RIGHT = gate opens
+    pins.servoWritePin(AnalogPin.P0, 90)
+    basic.showString(">")
 })
-basic.showString("Open")
+input.onGesture(Gesture.Shake, function () {
+    // Reset to middle
+    pins.servoWritePin(AnalogPin.P0, 45)
+    basic.showIcon(IconNames.Target)
+})
